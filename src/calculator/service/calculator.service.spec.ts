1fad9b7e65802dd293bd317c10322773a41c9757
ritesh <chaudharyritesh947@gmail.com>
2023-05-06 14:55:35 +0530

    Commiting the positive test cases

--- src/calculator/service/calculator.service.spec.ts
+++ src/calculator/service/calculator.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ICalculateService } from '../adaptor/service/ICalculateService';
 import { CalculatorService } from './calculator.service';
 
 describe('CalculatorService', () => {
@@ -11,8 +12,24 @@ describe('CalculatorService', () => {
 
     service = module.get<CalculatorService>(CalculatorService);
   });
+ 
+  describe('add', () => {
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
+
+    it('should return 0 for an empty string', () => {
+      expect(service.add('')).toBe(0);
+    });
+
+    it('should return the number for a single number', () => {
+      expect(service.add('1')).toBe(1);
+    });
+
+    it('should return the sum for two numbers', () => {
+      expect(service.add('1,2')).toBe(3);
+    });
+   
   });
 });
--- src/calculator/service/calculator.service.ts
+++ src/calculator/service/calculator.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Delimeter } from '../../utils/delimeter';
+import { ICalculateService } from '../adaptor/service/ICalculateService';
 
 @Injectable()
-export class CalculatorService {
-    stringCalculator(input: any) {
-        throw new Error('Method not implemented.');
+export class CalculatorService implements ICalculateService {
+    public add(numbers: string): number {
+        if (numbers === '') {
+          return 0;
+        }
+    
+
+        const delimiter = Delimeter.COMMA;
+    
+        const parsed = numbers.split(delimiter).map(Number);
+        this.validateNegatives(parsed);
+    
+        const sum = parsed.reduce((digit, cur) => digit + cur, 0);
+        return sum;  
     }
+    
+    
+      private validateNegatives(numbers: number[]): void {
+        const negatives = numbers.filter((n) => n < 0);
+        if (negatives.length > 0) {
+          throw new Error(`negatives not allowed: ${negatives.join(',')}`);
+        }
+      }
 }
